ad01d2911b063397efb756b37274f5eb1adca401
yureehh <fabbri.juri@gmail.com>
2024-08-19 16:12:49 +0200

    fix(pages): improved pages appearance and behaviours

--- src/pages/1_🏆_Leagues.py
+++ src/pages/1_🏆_Leagues.py
@@ -14,11 +14,17 @@ def plot_bet_number_percentage(data):
     """
     st.write("### Bet Number Percentage by League")
     bet_counts = data["League"].value_counts().reset_index()
-    bet_counts.columns = ["League", "Count"]
+    bet_counts.columns = ["League", "Bets Count"]
+    bet_counts = bet_counts.sort_values("League")  # Sort alphabetically
     fig = px.pie(
         bet_counts,
-        values="Count",
+        values="Bets Count",
         names="League",
+    )
+    fig.update_layout(
+        yaxis=dict(autorange="reversed"),
+        showlegend=True,
+        legend_title_text="Bets Count",
     )
     st.plotly_chart(fig, use_container_width=True)
     st.markdown(VERTICAL_SPACE, unsafe_allow_html=True)
@@ -32,11 +38,12 @@ def plot_profit_by_league(data):
         data (pd.DataFrame): The data frame containing the bets ledger.
     """
     st.write("### Profit by League")
-    profit_by_league = data.groupby("League")["Profit"].sum().reset_index()
+    profit_by_league = data.groupby("League").agg({"Profit": "sum"}).reset_index()
+    profit_by_league["Bets Count"] = (
+        data["League"].value_counts().reindex(profit_by_league["League"]).values
+    )
     profit_by_league["Profit"] = profit_by_league["Profit"].round(2)
-    profit_by_league = profit_by_league.sort_values(
-        by="Profit", ascending=False
-    )  # Optimize sorting for display
+    profit_by_league = profit_by_league.sort_values("League")  # Sort alphabetically
     fig = px.bar(
         profit_by_league,
         x="Profit",
@@ -48,6 +55,7 @@ def plot_profit_by_league(data):
         ),
         color_discrete_map={"#00CC96": "#00CC96", "#FF6692": "#FF6692"},
         text="Profit",
+        hover_data={"Profit": False, "Bets Count": True, "League": False},
     )
     fig.update_traces(texttemplate="%{text}", textposition="outside")
     fig.update_layout(showlegend=False, yaxis=dict(autorange="reversed"))
@@ -65,11 +73,12 @@ def plot_winrate_by_league(data):
     """
     st.write("### Winrate by League")
     data["Win"] = data["Result"].apply(lambda x: 1 if x == "W" else 0)
-    winrate_by_league = data.groupby("League")["Win"].mean().reset_index()
+    winrate_by_league = data.groupby("League").agg({"Win": "mean"}).reset_index()
+    winrate_by_league["Bets Count"] = (
+        data["League"].value_counts().reindex(winrate_by_league["League"]).values
+    )
     winrate_by_league["Winrate"] = (winrate_by_league["Win"] * 100).round(2)
-    winrate_by_league = winrate_by_league.sort_values(
-        by="Winrate", ascending=False
-    )  # Optimize sorting for display
+    winrate_by_league = winrate_by_league.sort_values("League")  # Sort alphabetically
     fig = px.bar(
         winrate_by_league,
         x="Winrate",
@@ -78,6 +87,7 @@ def plot_winrate_by_league(data):
         labels={"League": "", "Winrate": "Winrate %"},
         color_discrete_sequence=["#0057B8"],  # Darker blue for winrate
         text="Winrate",
+        hover_data={"Winrate": False, "Bets Count": True, "League": False},
     )
     fig.update_traces(texttemplate="%{text}%", textposition="outside")
     fig.update_layout(showlegend=False, yaxis=dict(autorange="reversed"))
@@ -94,14 +104,16 @@ def plot_roi_by_league(data):
         data (pd.DataFrame): The data frame containing the bets ledger.
     """
     st.write("### ROI by League")
-    roi_by_league = data.groupby("League").agg({"Profit": "sum", "Wager": "sum"})
+    roi_by_league = (
+        data.groupby("League").agg({"Profit": "sum", "Wager": "sum"}).reset_index()
+    )
+    roi_by_league["Bets Count"] = (
+        data["League"].value_counts().reindex(roi_by_league["League"]).values
+    )
     roi_by_league["ROI"] = (
         (roi_by_league["Profit"] / roi_by_league["Wager"]) * 100
     ).round(2)
-    roi_by_league = roi_by_league.reset_index()
-    roi_by_league = roi_by_league.sort_values(
-        by="ROI", ascending=False
-    )  # Optimize sorting for display
+    roi_by_league = roi_by_league.sort_values("League")  # Sort alphabetically
     fig = px.bar(
         roi_by_league,
         x="ROI",
@@ -111,6 +123,13 @@ def plot_roi_by_league(data):
         color=roi_by_league["ROI"].apply(lambda x: "#00CC96" if x > 0 else "#FF6692"),
         color_discrete_map={"#00CC96": "#00CC96", "#FF6692": "#FF6692"},
         text="ROI",
+        hover_data={
+            "League": False,
+            "Profit": False,
+            "Wager": False,
+            "ROI": False,
+            "Bets Count": True,
+        },
     )
     fig.update_traces(texttemplate="%{text}%", textposition="outside")
     fig.update_layout(showlegend=False, yaxis=dict(autorange="reversed"))
